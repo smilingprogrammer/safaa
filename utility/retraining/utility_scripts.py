371a412f226d9847320c8f32316e91d15eaa48cc
smilingprogrammer <abdulsoburoyewale@gmail.com>
2025-07-29 18:40:32 +0100

    feat(script): implemented training script in pipeline

--- utility/retraining/utility_scripts.py
+++ utility/retraining/utility_scripts.py
@@ -81,5 +81,6 @@ if __name__ == '__main__':
         dataset_path = os.path.abspath(train_data_path)
         data = pd.read_csv(dataset_path)
         agent.train_false_positive_detector_model(data["copyright"], data["falsePositive"])
-        agent.save('model/')
+        model_dir = os.path.join(base_path, 'model')
+        agent.save(model_dir)
         print("✅ Training completed and model saved.")
