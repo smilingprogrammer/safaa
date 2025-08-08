1b02334624b2589af5f8c7e0f40788c4d5175820
smilingprogrammer <abdulsoburoyewale@gmail.com>
2025-08-08 20:28:37 +0100

    feat(script): testing

--- utility/retraining/utility_scripts.py
+++ utility/retraining/utility_scripts.py
@@ -4,8 +4,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, f1_score, recall_score
-# from safaa.Safaa import SafaaAgent
-from Safaa.src.safaa.Safaa import SafaaAgent
+from safaa.Safaa import SafaaAgent
+# from Safaa.src.safaa.Safaa import SafaaAgent
 import os
 import glob
 import argparse
